aaf29fa7c16457b88e6f66ce6d0357f3c19ff74e
mch <k735michael@gmail.com>
2018-01-24 12:19:23 +0300

    Add nulls values

--- lib/wbench/timing_hash.rb
+++ lib/wbench/timing_hash.rb
@@ -1,12 +1,11 @@
 module WBench
   class TimingHash < Hash
     def initialize(hash)
-      # Remove 0 values as they indicate events that didn't occur
-      hash = hash.delete_if { |key, value| value == 0 }
-
       # Grab the start time and offset the values against it.
-      start_time = hash.min_by(&:last).last
-      hash = hash.map { |key, value| [key, value - start_time] }
+      start_time = hash.reject { |_, v| v == 0 }.min_by(&:last).last
+      hash = hash.map do |key, value|
+        [key, value != 0 ? value - start_time : value]
+      end
 
       # Order the results by value, lowest to highest
       hash.sort_by(&:last).each { |key, value| self[key] = value }
